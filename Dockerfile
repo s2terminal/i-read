ec6bfb6b732faf4ee0607adf2a0893c553cf6ba0
suzuki.sh <suzuki.sh@s2terminal.com>
2020-07-05 19:29:07 +0900

    execute command

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
 FROM rust:slim
 WORKDIR /app
 
+RUN apt-get update \
+  && apt-get upgrade -y \
+  && apt-get install -y \
+  git
+
 COPY ./ ./
 RUN cargo build
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::process::Command;
+use structopt::StructOpt;
+use regex::Regex;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    filename: std::path::PathBuf
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, i-read-u!");
+
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.filename).expect("No such file or directory");
+
+    // FIXME: 選択可能にする
+    let mut cmd_str = String::from("ls -l -a");
+    let cmd_pattern = Regex::new(r"^\s*[#>$]+\s*").unwrap();
+    for line in content.lines() {
+        if cmd_pattern.is_match(line) {
+            cmd_str = cmd_pattern.replace(line, "").into_owned();
+        }
+    }
+
+    println!("command: {}", cmd_str);
+
+    let mut result = Command::new("sh")
+        .arg("-c")
+        .arg(cmd_str)
+        .spawn()
+        .unwrap();
+    result.wait();
 }
