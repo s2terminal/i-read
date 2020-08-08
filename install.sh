384832ef7c7944f4ff49c9a3984363c11cfb52c2
s2terminal <suzuki.sh@s2terminal.com>
2020-08-08 16:32:48 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
 set -e
 cd /tmp
-IREADRS=./i-read-rs.tar.gz
-IREADRS_VERSION=`curl --silent https://api.github.com/repos/s2terminal/i-read-rs/releases | grep browser_download_url | awk -F'/' '{ print $8 }' | head -n 1`
-wget https://github.com/s2terminal/i-read-rs/releases/download/$IREADRS_VERSION/i-read-rs.tar.gz --output-document=$IREADRS
-tar --gunzip --extract --file $IREADRS
-rm $IREADRS
-chmod 755 ./i-read-rs
-mv ./i-read-rs /usr/local/bin/
-echo "Install $IREADRS_VERSION"
-unset IREADRS
-unset IREADRS_VERSION
+IREAD=./iread.tar.gz
+IREAD_VERSION=`curl --silent https://api.github.com/repos/s2terminal/i-read/releases | grep browser_download_url | awk -F'/' '{ print $8 }' | head -n 1`
+curl --location --remote-name https://github.com/s2terminal/i-read/releases/download/$IREAD_VERSION/iread.tar.gz
+tar --gunzip --extract --file $IREAD
+rm $IREAD
+chmod 755 ./iread
+mv ./iread /usr/local/bin/
+echo "Installed $IREAD_VERSION"
+unset IREAD
+unset IREAD_VERSION
