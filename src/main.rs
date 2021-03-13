5b8b7e5d825e04eb1d9eaa2183a7d67eb8054708
s2terminal <suzuki.sh@s2terminal.com>
2021-03-13 16:51:37 +0900

    set default theme

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,16 @@ mod exec;
 
 use cursive::traits::*;
 use cursive::views::{Dialog, SelectView};
+use cursive::theme::{Color, PaletteColor, Theme};
 use cursive::Cursive;
 
 fn main() {
     let content = args::get_content();
     let commands = cmds::Command::get_commands(content);
-    exec_command(commands)
+    select_and_exec_command(commands)
 }
 
-fn exec_command(commands: Vec<cmds::Command>) {
+fn select_and_exec_command(commands: Vec<cmds::Command>) {
     let mut select = SelectView::new().autojump();
 
     for command in commands {
@@ -25,16 +26,23 @@ fn exec_command(commands: Vec<cmds::Command>) {
     });
 
     let mut siv = cursive::default();
+    let theme = custom_theme_from_cursive(&siv);
+    siv.set_theme(theme);
 
     siv.add_layer(
         Dialog::around(select.scrollable()).title("choice command"),
     );
-
-    siv.set_user_data(exec_command);
 
     siv.run();
 
     let take_command: cmds::Command = siv.take_user_data().unwrap();
     println!("execute command: {}", take_command);
     exec::execute(&take_command.executable());
+}
+
+// https://github.com/gyscos/cursive/blob/cursive-v0.16.3/examples/src/bin/terminal_default.rs
+fn custom_theme_from_cursive(siv: &Cursive) -> Theme {
+    let mut theme = siv.current_theme().clone();
+    theme.palette[PaletteColor::Background] = Color::TerminalDefault;
+    theme
 }
